c885d5deee52796bb8eeab0c0f9ebff81825e3f7
Matt <matt@atomicbytes.com>
2019-08-18 11:20:57 -0500

    add emotion

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-const Home = () => <div>Home</div>;
+const PlayerCard = styled.div`
+  border-radius: 5px;
+  box-shadow: 0 0 10px gray;
+  padding: 15px;
+  margin: 5px;
+`;
+const Home = () => (
+  <div>
+    <PlayerCard>Player Foo</PlayerCard>
+  </div>
+);
 
 export default Home;
