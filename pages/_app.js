0c4bb3dce6bf8b7900b67c1d99207237d3928807
Matt <matt@atomicbytes.com>
2019-08-18 16:17:45 -0500

    change font

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,31 @@
 import App from "next/app";
+import Head from "next/head";
 import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import withApolloClient from "../lib/with-apollo-client";
+import { Global, css } from "@emotion/core";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
       <ApolloProvider client={apolloClient}>
+        <Head>
+          <link
+            href={`https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap`}
+            rel="stylesheet"
+            key="google-font-noto-sans-tc"
+          />
+        </Head>
+        <Global
+          styles={css`
+            * {
+              font-family: "Noto Sans TC", sans-serif;
+              color: #222;
+            }
+          `}
+        />
+
         <Component {...pageProps} />
       </ApolloProvider>
     );
